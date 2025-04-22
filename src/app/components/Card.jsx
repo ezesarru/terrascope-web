cb07b0b9685ef7f26bf7f987aa3cf6e21d81aaca
EzeSarru <ezesarru@gmail.com>
2025-04-22 17:30:59 -0300

    cards wip

--- src/app/components/Card.jsx
+++ src/app/components/Card.jsx
@@ -1,12 +1,14 @@
 export default function Card({ name, flag, continent, capital, population }) {
   return (
-    <div>
-      <img src={flag} alt="flag"></img>
-      <div>
-        <h2>{name}</h2>
-        <p>Capital: {capital}</p>
-        <p>Continent: {continent}</p>
-        <p>Population: {population}</p>
+    <div className="inline-block border w-52 m-4">
+      <div className="h-36 flex items-center">
+        <img className="" src={flag} alt="flag"></img>
+      </div>
+      <div className="p-4 space-y-2">
+        <h2 className="text-lg font-bold">{name}</h2>
+        <p className="text-sm">Capital: {capital}</p>
+        <p className="text-sm">Continent: {continent}</p>
+        <p className="text-sm">Population: {population}</p>
       </div>
     </div>
   );
